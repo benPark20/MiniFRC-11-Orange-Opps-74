d687a0c8aba916aa25693978a190532c726b08d4
Ben Park <152503119+benPark20@users.noreply.github.com>
2025-07-04 12:36:16 -0400

    Add files via upload

---
+++ MiniFRC11/constants.h
@@ -0,0 +1,60 @@
+#ifndef CONSTANTS
+#define CONSTANTS
+
+constexpr int x = 0; //placeholder
+
+//Buttons
+constexpr int BUTTON_BOTTOM = 0;
+constexpr int BUTTON_RIGHT = 1;
+constexpr int BUTTON_LEFT = 2;
+constexpr int BUTTON_TOP = 3;
+constexpr int LEFT_BUMPER = 4;
+constexpr int RIGHT_BUMPER = 5;
+constexpr int LEFT_TRIGGER = 6;
+constexpr int RIGHT_TRIGGER = 7;
+constexpr int MID_RIGHT = 8;
+constexpr int MID_LEFT = 9;
+constexpr int L_PRESS = 10;
+constexpr int R_PRESS = 11;
+constexpr int D_UP = 12;
+constexpr int D_DOWN = 13;
+constexpr int D_LEFT = 14;
+constexpr int D_RIGHT = 15;
+//Servo Angles
+//stow angle
+constexpr float stowI = 45;
+constexpr float stowII = 45;
+//l1
+constexpr float l1I = x;
+constexpr float l1II = x;
+//l2
+constexpr float l2I = x;
+constexpr float l2II = x;
+//l3
+constexpr float l3I = x;
+constexpr float l3II = x;
+//l4
+constexpr float l4I = x;
+constexpr float l4II = x;
+//algea l2
+constexpr float Al2I = x;
+constexpr float Al2II = x;
+//algae l3
+constexpr float Al3I = x;
+constexpr float Al3II = x;
+//score processor
+constexpr float processorI = x;
+constexpr float processorII = x;
+//score barge
+constexpr float bargeI = x;
+constexpr float bargeII = x;
+//claw intake Algea/stow
+constexpr float AI = x;
+//claw intake coral
+constexpr float CI = x;
+//claw grab Algea
+constexpr float AG = x;
+//claw grab Coral
+constexpr float CG = x;
+//
+#endif
